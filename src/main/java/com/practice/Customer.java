edf2d706c9c6047263452fe5758997dc26560ce5
MangelCG94 <mangelc13@gmail.com>
2024-10-13 00:58:38 +0200

    Añadido campo id al método toString en la clase Customer.

--- src/main/java/com/practice/Customer.java
+++ src/main/java/com/practice/Customer.java
@@ -51,6 +51,6 @@ public class Customer {
 
     @Override
     public String toString() {
-        return nombre + "\t" + apellido + "\t" + email;
+        return id + "\t" + nombre + "\t" + apellido + "\t" + email;
     }
 }
